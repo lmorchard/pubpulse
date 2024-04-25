fac1b17f641ffb55d79054a5fddbd4aec195f324
Les Orchard <lorchard@mozilla.com>
2024-04-25 16:07:23 -0700

    move more notebook config into env vars on startup to match mastodon_agent config

--- scripts/start-host-notebook.sh
+++ scripts/start-host-notebook.sh
@@ -5,6 +5,11 @@ export JUPYTER_CONFIG_DIR=notebook/config
 export JUPYTER_DATA_DIR=notebook/data
 export JUPYTER_RUNTIME_DIR=notebook/runtime
 
-export DATABASE_URL=postgresql://postgres:8675309jenny@localhost:55432/example
+export MASTODON_AGENT_DATABASE_URL=postgresql://postgres:8675309jenny@localhost:55432/example
+export MASTODON_AGENT_DEBUG=True
+export MASTODON_AGENT_EMBEDDINGS_API_URL=http://127.0.0.1:8674/predictions/my_model
+export MASTODON_AGENT_CELERY_BROKER_URL=amqp://localhost
+export MASTODON_AGENT_CELERY_RESULTS_BACKEND=rpc://localhost
+export MASTODON_AGENT_ML_API_URL=http://127.0.0.1:8673
 
 ./venv/bin/jupyter lab
